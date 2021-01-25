582eb5eeae3456c478b98f6b89f37b0d40697b09
JessFlexx <73108005+JessFlexx@users.noreply.github.com>
2021-01-25 14:27:03 +0300

    test

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -10,7 +10,7 @@ namespace OOP1
             product1.Id = 1;
             product1.CategoryId = 2;
             product1.ProductName = "Table";
-            product1.UnitPrice = 500;
+            product1.UnitPrice = 505;
             product1.UnitsInStock = 3;
 
             Product product2 = new Product { Id = 2, CategoryId = 5, 
