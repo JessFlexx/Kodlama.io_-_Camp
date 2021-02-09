9dff2b75b140d458667cf7acc4d967a3b622faf9
JessFlexx <73108005+JessFlexx@users.noreply.github.com>
2021-02-09 15:47:19 +0300

    Ürün hasılat ödevi

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿Select ProductName AS 'Ürün Adı' , SUM(Quantity*od.UnitPrice) AS 'Kazanılan Toplam Miktar'
+From Products p
+INNER JOIN [Order Details] od 
+ON p.ProductID=od.ProductID
+
+Group By ProductName;  --Ürünlerden Kazanalıan Miktarı Döndüren Kod.
